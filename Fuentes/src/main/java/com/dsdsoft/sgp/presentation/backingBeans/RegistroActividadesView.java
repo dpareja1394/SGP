81d4fa65152315eef16ec97e51d467191e1edeba
Daniel Pareja Londoño <dpareja1394@gmail.com>
2019-07-30 15:04:37 -0500

    Se comienza con pantalla para registro de actividades

---
+++ Fuentes/src/main/java/com/dsdsoft/sgp/presentation/backingBeans/RegistroActividadesView.java
@@ -0,0 +1,191 @@
+package com.dsdsoft.sgp.presentation.backingBeans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+
+import org.primefaces.component.selectonemenu.SelectOneMenu;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dsdsoft.sgp.modelo.TipoActividad;
+import com.dsdsoft.sgp.modelo.Usuario;
+import com.dsdsoft.sgp.presentation.businessDelegate.IBusinessDelegatorView;
+import com.dsdsoft.sgp.utilities.FacesUtils;
+
+/**
+ * @author Daniel Pareja Londoño
+ * @version jul. 30, 2019
+ * @since 1.8
+ */
+@ManagedBean
+@ViewScoped
+public class RegistroActividadesView implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = LoggerFactory.getLogger(RegistroActividadesView.class);
+
+	@ManagedProperty(value = "#{BusinessDelegatorView}")
+	private IBusinessDelegatorView businessDelegatorView;
+
+	private String usuarioIniciado;
+
+	private Usuario usuario;
+	
+	/**
+	 * Atributos entidad {Actividad}
+	 */
+	
+	private SelectOneMenu somTipoActividad;
+	private List<SelectItem> siTipoActividad;
+	
+
+	/**
+	 * @author Daniel Pareja Londoño
+	 * @version jul. 30, 2019
+	 * @since 1.8
+	 * @return <b>{@code }</b> Start here...
+	 */
+	public RegistroActividadesView() {
+		super();
+		usuarioIniciado = FacesUtils.getHttpSession(true).getAttribute("usuario_iniciado").toString();
+	}
+
+	/* Métodos y acciones */
+
+	/**
+	 *
+	 * @author Daniel Pareja Londoño
+	 * @version may. 28, 2019
+	 * @since 1.8
+	 * @return El/La businessDelegatorView
+	 *
+	 */
+	public IBusinessDelegatorView getBusinessDelegatorView() {
+		return businessDelegatorView;
+	}
+
+	/**
+	 *
+	 * @param businessDelegatorView El/La businessDelegatorView a setear
+	 * @author Daniel Pareja Londoño
+	 * @version may. 28, 2019
+	 * @since 1.8
+	 *
+	 */
+	public void setBusinessDelegatorView(IBusinessDelegatorView businessDelegatorView) {
+		this.businessDelegatorView = businessDelegatorView;
+	}
+
+	/**
+	 *
+	 * @author Daniel Pareja Londoño
+	 * @version may. 28, 2019
+	 * @since 1.8
+	 * @return El/La usuarioIniciado
+	 *
+	 */
+	public String getUsuarioIniciado() {
+		return usuarioIniciado;
+	}
+
+	/**
+	 *
+	 * @param usuarioIniciado El/La usuarioIniciado a setear
+	 * @author Daniel Pareja Londoño
+	 * @version may. 28, 2019
+	 * @since 1.8
+	 *
+	 */
+	public void setUsuarioIniciado(String usuarioIniciado) {
+		this.usuarioIniciado = usuarioIniciado;
+	}
+
+	/**
+	 *
+	 * @author Daniel Pareja Londoño
+	 * @version may. 28, 2019
+	 * @since 1.8
+	 * @return El/La usuario
+	 *
+	 */
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 *
+	 * @param usuario El/La usuario a setear
+	 * @author Daniel Pareja Londoño
+	 * @version may. 28, 2019
+	 * @since 1.8
+	 *
+	 */
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 *
+	 * @author Daniel Pareja Londoño
+	 * @version jul. 30, 2019
+	 * @since 1.8
+	 * @return El/La somTipoActividad
+	 *
+	 */
+	public SelectOneMenu getSomTipoActividad() {
+		return somTipoActividad;
+	}
+
+	/**
+	 *
+	 * @param somTipoActividad El/La somTipoActividad a setear
+	 * @author Daniel Pareja Londoño
+	 * @version jul. 30, 2019
+	 * @since 1.8
+	 *
+	 */
+	public void setSomTipoActividad(SelectOneMenu somTipoActividad) {
+		this.somTipoActividad = somTipoActividad;
+	}
+
+	/**
+	 *
+	 * @author Daniel Pareja Londoño
+	 * @version jul. 30, 2019
+	 * @since 1.8
+	 * @return El/La siTipoActividad
+	 *
+	 */
+	public List<SelectItem> getSiTipoActividad() {
+		try {
+			if(siTipoActividad == null) {
+				siTipoActividad = new ArrayList<SelectItem>();
+				List<TipoActividad> tiposActividad = businessDelegatorView.getTipoActividad();
+				for (TipoActividad tipoActividad : tiposActividad) {
+					siTipoActividad.add(new SelectItem(tipoActividad.getTiacId(), tipoActividad.getDescripcionTiact()));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return siTipoActividad;
+	}
+
+	/**
+	 *
+	 * @param siTipoActividad El/La siTipoActividad a setear
+	 * @author Daniel Pareja Londoño
+	 * @version jul. 30, 2019
+	 * @since 1.8
+	 *
+	 */
+	public void setSiTipoActividad(List<SelectItem> siTipoActividad) {
+		this.siTipoActividad = siTipoActividad;
+	}
+
+}
---
+++ Ingenieria/Scripts/InsertTipoActividad.sql
@@ -0,0 +1,8 @@
+insert into tipo_actividad(descripcion_tiact, nombre_corto) values('Desarrollo','DEV');
+insert into tipo_actividad(descripcion_tiact, nombre_corto) values('Arquitectura','ARQ');
+insert into tipo_actividad(descripcion_tiact, nombre_corto) values('Pruebas','TST');
+insert into tipo_actividad(descripcion_tiact, nombre_corto) values('Soporte','SOP');
+insert into tipo_actividad(descripcion_tiact, nombre_corto) values('Despliegues','DPL');
+insert into tipo_actividad(descripcion_tiact, nombre_corto) values('Infraestructura','INF');
+insert into tipo_actividad(descripcion_tiact, nombre_corto) values('Requisitos','REQ');
+insert into tipo_actividad(descripcion_tiact, nombre_corto) values('Reunión','REU');
